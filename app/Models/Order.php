7623b4ac01a92f26b50e3e5a499304dd0fcbb45b
Jong44 <mat.skak45@gmail.com>
2026-02-26 22:21:30 +0700

    feat:add soft delete at models: paymentMethod, order, review, voucher, ticketType

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Order extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
     protected $casts = [
         'total_harga' => 'decimal:2',
         'diskon_amount' => 'decimal:2',
